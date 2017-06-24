6844be277020091a01082b55d3aed7c5fb526afc
Ruben Torres <ruben.torres@huawei.com>
2017-06-23 19:07:37 -0500

    Created an object oriented application

--- main.py
+++ main.py
@@ -3,106 +3,120 @@ from sqlite3 import connect
 from selenium import webdriver
 from datetime import datetime
 
-url = "https://www.bolsadevaloresguayaquil.com/acciones/bvg.asp"
-driver = webdriver.Chrome()
-db = connect("D:/myScripts/stockstracer/db.db")
+class App:
+    def __init__(self):
+        self.url = "https://www.bolsadevaloresguayaquil.com/acciones/bvg.asp"
+        self.testWeekend()
+        self.connectToDb()
 
-def main():
-    if weekend():
-        quit()
+    def testWeekend(self):
+        date = datetime.today()
+        day = datetime.strftime(date, "%A")
+        if day in ["Sunday"]:
+            quit()
 
-    #download stock prices
-    download()
+    def connectToDb(self):
+        self.db = connect("D:/myScripts/stockstracer/db.db")
 
-    #calculate returns
-    for c in ["favorita", "holcim", "banco guayaquil", "banco pichincha", "cerveceria nacional",
-              "continental tire", "produbanco"]:
-        setReturn(c)
+    def createDriver(self):
+        driver = webdriver.Chrome()
+        self.driver = driver
 
-    db.commit()
-    db.close()
+    def download(self):
+        return #IMPLEMENT USING REQUESTS
 
-def weekend():
-    # do not run on weekends
-    day = datetime.today().strftime("%A")
-    return day in ["Saturday", "Sunday"]
-
-
-def download():
-    #downloads the site info and inserts it in the database
-    page_text = get_page_data()
-    stocks_list = format_data(page_text)
-    for s in stocks_list:
-        print s
-
-    insert_database(stocks_list)
-
-    driver.close()
-    driver.quit()
-
-def get_page_data():
-    #dowloads the pade
-    driver.get(url)
-    table = driver.find_element_by_tag_name("table")
-    page_text = table.text
-    return page_text
+    def downloadWithDriver(self):
+        # dowloads the pade
+        self.driver.get(self.url)
+        table = self.driver.find_element_by_tag_name("table")
+        self.page_text = table.text
 
 
-def format_data(page_text):
-    #takes the page and returns a list of dictionaries with stock info
-    today = datetime.today().strftime("%Y-%m-%d")
-    stocks_list= []
+    def format_data(self):
+        # takes the page and returns a list of dictionaries with stock info
+        today = datetime.today().strftime("%Y-%m-%d")
+        stocks_list = []
 
-    stock_blocks = page_text.split("\n")[4:-3]
+        stock_blocks = self.page_text.split("\n")[4:-3]
 
-    #define the regexps
-    companyreg = re.compile(r'\D*')
-    stockreg = re.compile(r'\$\d+[\.\,]?\d+[\.\,]?\d*')
+        # define the regexps
+        companyreg = re.compile(r'\D*')
+        stockreg = re.compile(r'\$\d+[\.\,]?\d+[\.\,]?\d*')
 
-    for s in stock_blocks:
-        find_company = companyreg.search(s)
-        find_price = stockreg.search(s)
+        for s in stock_blocks:
+            find_company = companyreg.search(s)
+            find_price = stockreg.search(s)
 
-        if find_company and find_price:
-            company = find_company.group()
-            price = find_price.group()[1:]
-            price = price.replace(",", "")
-        else:
-            continue
+            if find_company and find_price:
+                company = find_company.group().strip()
+                price = find_price.group()[1:]
+                price = price.replace(",", "")
+            else:
+                continue
 
-        if company and price:
-            stocks_list.append({
-                "date": today,
-                "company": company,
-                "price": float(price)
-            })
+            if company and price:
+                stocks_list.append({
+                    "date": today,
+                    "company": company,
+                    "price": float(price)
+                })
 
-    return stocks_list
+        self.stocks_list = stocks_list
 
-def insert_database(data):
-    for d in data:
-        db.execute("insert into stocks (date, company, price) values (?,?,?)", (d["date"], d["company"], d["price"]))
+    def insert_database(self):
+        for d in self.stocks_list:
+            self.db.execute("insert into stocks (date, company, price) values (?,?,?)",
+                       (d["date"], d["company"], d["price"]))
 
 
 
-def setReturn(companyName):
-    #calculates the expected return of a stock at current price
-    cursor = db.execute(
-        'select rowid, date, company, price, value  from stocks where company like "%' + companyName + '%"')
-    for c in cursor:
-        rid = c[0]
-        date = c[1]
-        year = int(date[:4])
-        price = float(c[3])
-        dividendQuery = db.execute(
-            'select dividend from expected_dividends where company like "%' + companyName + '%"' + 'and year = ?',
-            (year,))
-        expected_dividend = dividendQuery.fetchone()
-        if expected_dividend:
-            print expected_dividend[0], price
-            expected_return = expected_dividend[0] / price
-            db.execute("update stocks set value = ? where rowid = ?", (expected_return, rid))
+    def setReturn(self, companyName):
+        # calculates the expected return of a stock at current price
+        cursor = self.db.execute(
+            'select rowid, date, company, price, return  from stocks where company like "%' + companyName + '%"')
+        for c in cursor:
+            rid = c[0]
+            date = c[1]
+            year = int(date[:4])
+            price = c[3]
+            dividendQuery = self.db.execute(
+                'select dividend from expected_dividends where company like "%' + companyName + '%"' + 'and year = ?',
+                (year,))
+            expected_dividend = dividendQuery.fetchone()
+            if expected_dividend:
+                print expected_dividend[0], price
+                expected_return = expected_dividend[0] / price
+                self.db.execute("update stocks set return = ? where rowid = ?", (expected_return, rid))
+
+    def setYield(self, companyName):
+        cursor = self.db.execute(
+            'select rowid, date, company, price, earnings_yield  from stocks where company like "%' + companyName + '%"')
+        for c in cursor:
+            rid = c[0]
+            date = c[1]
+            year = int(date[:4])
+            price = c[3]
+            earningsQuery = self.db.execute(
+                'select earnings_per_share from earnings where company like "%' + companyName + '%"' + 'and year = ?',
+                (year,))
+            earnings = earningsQuery.fetchone()
+            if earnings:
+                print earnings[0], price
+                earnings_yield = earnings[0] / price
+                self.db.execute("update stocks set earnings_yield = ? where rowid = ?", (earnings_yield, rid))
+
+    def calculateReturs(self):
+        for c in ["favorita", "holcim", "banco guayaquil", "banco pichincha", "cerveceria nacional",
+                  "continental tire", "produbanco", "homeforest", "alicosta", "cerro verde", "brikapital",
+                  "banco bolivariano", "unacem", "pathforest", "el tecal", "tonicorp", "inversancarlos"]:
+            self.setReturn(c)
+            self.setYield(c)
 
 
 if __name__ == "__main__":
-    main()
+    app = App()
+    app.downloadWithDriver()
+    app.format_data()
+    app.insert_database()
+    app.db.commit()
+    app.db.close()
